f353c0acd03bde17b1fd40300e6a696b966fac83
Yeongchang Cho <yeongchang.cho@gmail.com>
2022-10-26 17:02:36 +0900

    load memory by time

--- tsff/data_module/pipelines/load_memory_time.py
+++ tsff/data_module/pipelines/load_memory_time.py
@@ -9,22 +9,27 @@ import pandas as pd
 from ..utils.builder import build_pipeline,PIPELINES
 
 @PIPELINES.register_module()
-class load_memory:
-    def __init__(self,pairs,data_path,headers,start_time,end_time):
+class load_memory_time:
+    def __init__(self,pairs,data_path,headers,start_time,end_time,data_type = 'klines'):
         self.pairs = pairs
         self.data_path = data_path
         self.headers = headers
         self.start_time = datetime.strptime(start_time , "%Y%m%d")
         self.end_time = datetime.strptime(end_time, "%Y%m%d")
+        self.data_type = data_type
 
     def __call__(self):
 
-        def load_df_m_kl(dates,pair,_data_path,headers):
+        def load_df_m(dates,pair,_data_path,headers):
             assert len(dates) > 0
             cur_dfs = []
             for date in dates:
-                cur_date = f"{pair}-1m-{date[:4]}-{date[4:6]}.csv"
-                data_path = os.path.join(_data_path,pair,'1m',cur_date)
+                if self.data_type == 'klines':
+                    cur_date = f"{pair}-1m-{date[:4]}-{date[4:6]}.csv"
+                    data_path = os.path.join(_data_path,pair,'1m',cur_date)
+                elif self.data_type == 'aggtrades':
+                    cur_date = f"{pair}-aggTrades-{date[:4]}-{date[4:6]}.csv"
+                    data_path = os.path.join(_data_path,pair,cur_date)
                 cur_df = pd.read_csv(data_path)
                 cur_df.columns = headers 
                 cur_dfs.append(cur_df)
@@ -41,7 +46,7 @@ class load_memory:
             start_date = date
 
         for pair in self.pairs:
-            df_dict.update({pair : load_df_m_kl(dates,pair,self.data_path,self.headers)})
+            df_dict.update({pair : load_df_m(dates,pair,self.data_path,self.headers)})
         
         data = df_dict
 
